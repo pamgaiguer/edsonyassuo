3f99ed2d508a96b91373528a22ce73866427ced3
Pamella Gaiguer Santos <pamella.santos@totvs.com.br>
2015-11-03 11:52:10 -0200

    adicionando

---
+++ _header.php
@@ -0,0 +1,15 @@
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Edson Yassuo - Videografista | Designer | Animador</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="css/simple-sidebar.css"> -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/typography.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="icon" href="images/favicon.ico">
+</head>
---
+++ _sidemenu.php
@@ -0,0 +1,26 @@
+            <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                <div id="sidebar-wrapper">
+                    <div class="logo">
+                        <img src="images/logo.png" class="center-block">
+                        <h4 class="text-center">Edson Yassuo</h4>
+                        <p class="text-center">Videografista | Designer | Animador</p>
+                    </div>
+                    <div class="side-menu">
+                        <ul class="list-unstyled">
+                            <a href="work-all.html"><li class="text-center center-block">Trabalhos</li></a>
+                            <a href="about.html"><li class="text-center center-block">Sobre</li></a>
+                            <a href="contact.html"><li class="text-center center-block">Contato</li></a>
+                        </ul>
+                    </div>
+                    <div class="side-tags">
+                        <legend>TAGS</legend>
+                        <ul class="list-unstyled">
+                            <li class="text-left"><a href="#">Todos</a></li>
+                            <li class="text-left"><a href="#">Animação</a></li>
+                            <li class="text-left"><a href="#">Design</a></li>
+                            <li class="text-left"><a href="#">Edição de Vídeo</a></li>
+                        </ul>
+                    </div>
+                    <p class="text-center">Copyright &copy; <a href="http://www.pamellagaiguer.com" target="_blank">Pam Gaiguer</a></p>
+                </div>
+            </div>
